1ce0b43d8b74a878da044c204ee0905f7d75dde7
root <root@Isra>
2024-10-29 21:45:56 -0400

    add files

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import sys
+import Command from 
 
 def repl():
     while True:
         val = input("Enter input: ")
+        userInput = val.split()  
+        cmd, num1, num2 = userInput[0], userInput[1], userInput[2]
+
+        commandList = {
+            "add": AddCommand, 
+            "subtract": SubtractCommand, 
+            "multiply": MultiplyCommand, 
+            "divide": DivideCommand 
+        }
+
+
+        try:
+            commandClass = commandList[cmd]
+            commandClass.execute(self, params=(num1, num2))
+        except KeyError:
+            print("Invalid Command")
+        except Exception:
+            print("Execution failed")
 
 if __name__ == "__main__":
     repl()
